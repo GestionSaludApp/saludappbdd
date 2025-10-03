19d77d8d715e09476d81da721eb626bdbe7533ee
GestionSaludApp <GestionSaludApp@gmail.com>
2025-10-03 13:09:14 -0300

    creds2

---
+++ credenciales.js
@@ -0,0 +1,17 @@
+export const credenciales = {
+  mysql: {
+    host: 'mysql.db.mdbgo.com',
+    user: 'saludapp_admin',
+    password: 'Practica3!',
+    database: 'saludapp_bdd'
+  },
+  cloudinary: {
+    cloud_name: 'NOMBRE_REPO',
+    api_key: 'KEY',
+    api_secret: 'SECRETO'
+  },
+  email: {
+    usuario: "gestionSaludApp@email.com",
+    password: ""
+  }
+};
