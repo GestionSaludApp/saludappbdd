0bf1b3028d2eaa104517e2328ce575dbac1c1a73
GestionSaludApp <GestionSaludApp@gmail.com>
2025-10-06 10:26:26 -0300

    cred14

--- credenciales.js
+++ credenciales.js
@@ -11,7 +11,7 @@ const credenciales = {
     api_secret: 'i5Zis5pawFJdFpyHFKHMi60T3GQ'
   },
   email: {
-    usuario: 'gestionsaludapp@gmail.com',
+    usuario: 'gestionsaludapp',
     password: 'gqex tfze cfmr lacg'
   }
 };
