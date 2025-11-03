f434cdb57189c26ddfbd1e2631a52c053deea715
GestionSaludApp <gestionSaludApp@gmail.com>
2025-11-03 00:11:26 -0300

    fixEmail

--- bddUsuario.js
+++ bddUsuario.js
@@ -4,6 +4,9 @@ const { credenciales } = require("./credenciales.js");
 
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   service: "gmail",
   auth: {
     user: credenciales.email.usuario,
